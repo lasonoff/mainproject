b7c96184428e83b0499b79114a94ce6e10126d66
lasonoff <lason-vrn@yandex.ru>
2021-05-03 19:44:31 +0300

    Для миграции БД используется liquibase

--- src/main/java/ru/yauroff/test/mainproject/Main.java
+++ src/main/java/ru/yauroff/test/mainproject/Main.java
@@ -1,7 +1,6 @@
 package ru.yauroff.test.mainproject;
 
 import ru.yauroff.test.mainproject.common.PropertiesManager;
-import ru.yauroff.test.mainproject.repository.DbCreator;
 import ru.yauroff.test.mainproject.view.MainView;
 
 public class Main {
@@ -9,8 +8,6 @@ public class Main {
     public static void main(String[] args) {
         // Загрузка настроек
         PropertiesManager.getInstance();
-        // Создание структуры базы
-        DbCreator.createTables();
         // Запуск приложения2
         MainView mv = new MainView();
         mv.show();
---
+++ src/test/java/ru/yauroff/test/mainproject/EmptyTest.java
@@ -0,0 +1,11 @@
+package ru.yauroff.test.mainproject;
+
+
+import org.junit.Test;
+
+public class EmptyTest {
+    @Test
+    public void emptyTest() {
+
+    }
+}
